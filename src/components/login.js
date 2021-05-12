bfe575cb272f497ede3d32498c189edfac6b70aa
מיכל בוקסבוים <322368507@mbydomain>
2021-05-12 15:22:40 +0300

    Add login

---
+++ src/components/login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Login(){
+    return( <div>
+        <lable>
+        <input></input>שם
+        </lable>
+        <br></br>
+        <lable>
+        <input></input>סיסמה
+        </lable>
+        <br>
+        </br>
+        <button>כניסה</button>
+    </div>)
+}
+export default Login;
+
+
---
+++ src/components/registersHomePage.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function RegistersHomePage(){
+    return <div>
+        <div>
+           <lable>פניה חדשה</lable>
+           <lable>מספר זהות
+               <input></input>
+           </lable>
+           <br></br>
+
+             <lable>מספר זהות
+               <input></input>
+           </lable>
+           <br></br>   
+           <lable>שם פרטי
+               <input></input>
+           </lable>
+           <br></br>   
+           <lable>שם משפחה
+               <input></input>
+           </lable>
+           <br></br>   
+           <lable>מייל
+               <input></input>
+           </lable>
+           <br></br>   
+           <lable>שם הקורס
+               <input></input>
+           </lable>
+           <br></br>  
+           <lable>מקור פרסום
+               <input></input>
+           </lable>
+           <br></br>        
+            
+            <br></br>
+
+        </div>
+    </div>
+
+}
+export default RegistersHomePage;
