f36bf12ed3c591071a04bbee19d62ba5912b2f9a
TamarSharvit <tamar92932@gmail.com>
2021-06-09 15:24:51 +0300

    ff

--- src/components/login.js
+++ src/components/login.js
@@ -5,7 +5,6 @@ import ValidateTextField from '../common/validateTextFields';
 import { loginToServer, signup } from '../api/loginApi';
 
 const Login = () => {
-<<<<<<< HEAD
 
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
@@ -15,32 +14,11 @@ const Login = () => {
         //Validation - if email and pwd are VALID 
         signup(userName, password).then((res) => {
             debugger
-=======
-    const cheackValidName=()=>{
-        return userName === ''; 
-    }
-    const cheackValidPass=()=>{
-        return password.length < 6 ;
-    }
-    const history = useHistory();
-    const handleClick = () => {
-        if(!(cheackValidName()&&cheackValidPass())){
-        loginToServer(userName, password)/*send params*/.then((res) => {
-            //save token
->>>>>>> 29a46d235a416515bf0d36748734d9e9f60e5320
             history.push("/registersHomePage");
         }).catch(() => {
             //Handle error
         })
     }
-<<<<<<< HEAD
-
-=======
-}
-
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
->>>>>>> 29a46d235a416515bf0d36748734d9e9f60e5320
 
     return (<div>
         <div>שם משתמש</div>
