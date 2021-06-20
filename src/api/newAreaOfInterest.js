7e501638c113a583138e05d74052f44c2ab5eb4b
TamarSharvit <tamar92932@gmail.com>
2021-06-21 00:27:47 +0300

    add function to server but not run!!

---
+++ src/api/newAreaOfInterest.js
@@ -0,0 +1,16 @@
+
+
+export const addInterestToData = (priceDiscount, discount, price, modol, course, status) => {
+    debugger;
+    return fetch(`http://localhost:4000/newRegisters/addRegisterToData`, {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({ priceDiscount, discount, price, modol, course, status })
+    })
+        .then(() => {console.log("you add") })
+        .catch((err) => {
+            console.log(err);
+        })
+}
--- src/api/newRegisterApi.js
+++ src/api/newRegisterApi.js
@@ -3,12 +3,16 @@ import React from 'react';
 
 export const addRegisterToData = ( id, fName, lName, email, phone, sex, leadSource, additionalPhone, foreignBirthDate , hebrowBirthDate, note) => {
     debugger;
-    return fetch(`http://localhost:4000/addRegisterToData`, {
+    return fetch(`http://localhost:4000/newRegisters/addRegisterToData`, {
         method: 'POST', 
-        body: {  id, fName, lName, email, phone, sex, leadSource, additionalPhone, foreignBirthDate , hebrowBirthDate, note }
+        headers: {
+            'Content-Type': 'application/json'
+          },
+        body: JSON.stringify({  id, fName, lName, email, phone, sex, leadSource, additionalPhone, foreignBirthDate , hebrowBirthDate, note })
     })
         .then(() => {console.log("you add") })
         .catch((err) => {
             console.log(err);
         })
 }
+
